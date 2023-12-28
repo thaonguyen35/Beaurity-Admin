d9b18bc32af6aef0250cab00132dfb8be6fa7184
thaonguyen35 <154658764+thaonguyen35@users.noreply.github.com>
2023-12-28 22:58:49 +0700

    Update admin-home.component.ts

--- my-admin/src/app/admin-home/admin-home.component.ts
+++ my-admin/src/app/admin-home/admin-home.component.ts
@@ -1,14 +1,8 @@
-import { Component, ViewChild, ElementRef, VERSION } from '@angular/core';
-import { AdminCosmeticService } from '../services/admin-cosmetic.service';
-import { AdminCategoryService } from '../services/admin-category.service';
-import { AdminCustomerService } from '../services/admin-customer.service';
-import { AdminOrderService } from '../services/admin-order.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
   styleUrls: ['./admin-home.component.css']
 })
-export class AdminHomeComponent {
-  
-  }
+export class AdminHomeComponent {}
